91dbce98b15c80324dfccd44b3e66b5ea7129a7f
bania-bigmir <barhanof@gmail.com>
2018-02-28 13:38:04 +0200

    bug

--- resources/views/pink/gallery_content.blade.php
+++ resources/views/pink/gallery_content.blade.php
@@ -1,66 +1,38 @@
-<div class="post-wrap">
-    @if($galleries)
-        @foreach($galleries as $gallery)
+@if($gallery)
+    <h2>{{$gallery->name}}</h2>
+    <p>{{$gallery->description}}</p>
+    <div id="slider" class="flexslider">
+        <ul class="slides">
+    @foreach($gallery->photo as $photo)
 
-            @if($gallery->active)
-                <div class="flat-team list  has-image">
-                    <div class="team-image">
-                        <img src="{{ asset('images/galleries/'.$gallery->alias.'/'.$gallery->preview) }}"
-                             alt="{{ $gallery->name }}" style="width:275px;height: 242px">
-                    </div>
+        @if($photo->active)
 
-                    <div class="team-info">
-                        <div class="team-subtitle">{{ $gallery->created_at->format('d')}}&nbsp;{{ Lang::get('month.'.$gallery->created_at->format('m')) }}&nbsp;{{ $gallery->created_at->format('Y') }}</div>
-                        <h3 class="team-name">{{ $gallery->name }}</h3>
-                        <div class="team-desc">{{ $gallery->description }}</div>
+            <li data-thumb="{{asset('images/galleries')}}/{{$gallery->alias}}/{{$photo->url}}">
+                    <img src="{{asset('images/galleries')}}/{{$gallery->alias}}/{{$photo->url}}" alt="{{ $photo->name }}"/>
+
+            </li>
+
+        @endif
+    @endforeach
+        </ul>
+    </div>
+    <div id="carousel" class="flexslider">
+    <ul class="slides">
+    @foreach($gallery->photo as $photo)
+
+        @if($photo->active)
+
+            <li data-thumb="{{asset('images/galleries')}}/{{$gallery->alias}}/{{$photo->url}}">
+                <img src="{{asset('images/galleries')}}/{{$gallery->alias}}/{{$photo->url}}" alt="{{ $photo->name }}"/>
+
+            </li>
+
+        @endif
+    @endforeach
+    </ul>
+    </div>
+@endif
 
 
-                        @foreach($gallery->filters  as  $filter)
-
-                            <a href="{{ $filter['name'] }}">{{ $filter['name'] }}</a>,&nbsp;
-                        @endforeach
 
 
-                        <p class="box-readmore">
-                            <a href="{{ route('galereja.show',['alias'=>$gallery->alias]) }}">Дивитись галерею</a>
-                        </p>
-                    </div>
-                </div>
-            @endif
-        @endforeach
-    @endif
-
-        <div class="blog-pagination">
-            @if($galleries->lastPage() > 1)
-                <ul class="flat-pagination clearfix">
-                    @if($galleries->currentPage() !== 1 )
-                        <li class="prev">
-                        @if($galleries->currentPage() == 2)
-                                <a href="{{ asset('galereja') }}"><i
-                                            class="fa fa-angle-left fa-4x"></i></a>
-                        @else
-                            <a href="{{ $galleries->url($galleries->currentPage() - 1) }}"><i
-                                        class="fa fa-angle-left fa-4x"></i></a>
-                        </li>
-                        @endif
-                    @endif
-                    @for($i = 1; $i <= $galleries->lastPage();$i++)
-                        @if($galleries->currentPage() == $i)
-                            <li class="active disabled">{{ $i }}</li>
-                        @else
-                            @if($i==1)
-                            <li><a href="{{ asset('galereja') }}">{{$i}}</a></li>
-                                @else
-                                    <li><a href="{{ $galleries->url($i) }}">{{$i}}</a></li>
-                                @endif
-                        @endif
-                    @endfor
-                    @if($galleries->currentPage() !== $galleries->lastPage())
-                        <li class="next">
-                            <a href="{{ $galleries->url($galleries->currentPage()+1) }}"><i class="fa fa-angle-right fa-4x"></i></a>
-                        </li>
-                    @endif
-                </ul><!-- /.flat-pagination -->
-            @endif
-        </div><!-- /.blog-pagination -->
-</div>
